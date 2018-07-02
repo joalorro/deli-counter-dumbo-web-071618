3795e98a6dfe804932cd6fca7914214eab120988
Josue V. Alorro <joalorro@gmail.com>
2018-07-02 20:26:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,11 @@
 def line(katzDeli)
   place = 1
+  msg = "The line is currently: "
   if katzDeli.length == 0
     puts "The line is currently empty."
   else
     katzDeli.each do |name|
-      puts "#{place}. #{name}"
+      puts msg + "#{place}. #{name}"
       place += 1 
     end
   end
@@ -13,12 +14,12 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "#{name} is now number #{katz_deli.lenth} in line."
+  puts "#{name} is now number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
   if katz_deli.length > 0 
-    puts "Now serving #{katz_deli.first}."
+    puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   else 
     puts "There is nobody waiting to be served!"
